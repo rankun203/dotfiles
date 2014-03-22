237862859058f3ee5c5d13b03c7c425b5f01bf15
Kun Ran <rankun203@gmail.com>
2014-03-22 19:52:15 +0800

    add example.desktop for ubuntu

--- conf/cmus/playlist.pl
+++ conf/cmus/playlist.pl
@@ -3,3 +3,4 @@
 /home/rankun203/Music/老狼/万物生长 高晓松作品集/00 情人劫.mp3
 /home/rankun203/Music/老狼/万物生长 高晓松作品集/00 睡在我上铺的兄弟.mp3
 /home/rankun203/Music/Compilations/Moon River & Me/00 炎のたからもの 鲁邦三世原唱.mp3
+/home/rankun203/Music/Compilations/Anthology/00 Never Grow Old.flac
